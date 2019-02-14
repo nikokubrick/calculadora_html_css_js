2a91adda3d2c31e4ffb74e108259180a74d4b169
Saavedra <nicolas.saavedra@sofse.gob.ar>
2019-02-14 16:00:48 -0300

    arreglo del script

--- js/script.js
+++ js/script.js
@@ -93,19 +93,19 @@ function showResult() {
     let mult = actual.indexOf("x");
     if (suma !== -1) {
         arr = actual.split("+",2);
-        res = parseInt(arr[0]) + parseInt(arr[2]);
+        res = parseInt(arr[0]) + parseInt(arr[1]);
         document.getElementById("resultado").innerHTML = res;
     } else if (resta !== -1) {
         arr = actual.split("-",2);
-        res = arr[0] - arr[2];
+        res = arr[0] - arr[1];
         document.getElementById("resultado").innerHTML = res;
     } else if (div !== -1) {
         arr = actual.split("÷",2);
-        res = arr[0] / arr[2];
+        res = arr[0] / arr[1];
         document.getElementById("resultado").innerHTML = res;
     } else if (mult !== -1) {
         arr = actual.split("x",2);
-        res = arr[0] * arr[2];
+        res = arr[0] * arr[1];
         document.getElementById("resultado").innerHTML = res;
 
     }
